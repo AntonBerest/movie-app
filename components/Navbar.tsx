a1076af9ab201023374cb45906f090ac6a77e889
AntonBerest <berestovsky.sv@gmail.com>
2026-06-02 01:45:42 +0100

    movie page (links & nav)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,37 @@
-export default function Navbar() {
-  return (
-    <nav className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between px-16 py-5 bg-black/80 backdrop-blur-sm">
-      <div className="text-red-500 font-bold text-xl"> StreamVibe</div>
+'use client'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
 
-      <div className="bg-zinc-900 border border-zinc-800 rounded-xl px-8 py-4">
-        <ul className="hidden md:flex gap-8 text-white text-sm list-none">
-          <li className="cursor-pointer hover:text-red-500">Home</li>
-          <li className="cursor-pointer hover:text-red-500">Movies & Shows</li>
-          <li className="cursor-pointer hover:text-red-500">Support</li>
-          <li className="cursor-pointer hover:text-red-500">Subscriptions</li>
+export default function Navbar() {
+  const pathname = usePathname()
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between px-16 py-2 bg-transparent backdrop-blur-sm">
+      <div className="text-white font-bold text-xl"> StreamVibe</div>
+
+      <div className="bg-zinc-900 border border-zinc-800 rounded-xl px-4 py-3">
+        <ul className="flex gap-2 text-white text-sm list-none">
+          <Link
+            href="/"
+            className={`cursor-pointer px-4 py-2 rounded-lg ${pathname === '/' ? 'bg-zinc-800' : 'hover:bg-zinc-800'}`}
+          >
+            Home
+          </Link>
+          <Link
+            href="/movies/123"
+            className={`cursor-pointer px-4 py-2 rounded-lg ${pathname.startsWith('/movies') ? 'bg-zinc-800' : 'hover:bg-zinc-800'}`}
+          >
+            Movies & Shows
+          </Link>
+          <li
+            className={`cursor-pointer px-4 py-2 rounded-lg ${pathname === '/support' ? 'bg-zinc-800' : 'hover:bg-zinc-800'}`}
+          >
+            Support
+          </li>
+          <li
+            className={`cursor-pointer px-4 py-2 rounded-lg ${pathname === '/subscriptions' ? 'bg-zinc-800' : 'hover:bg-zinc-800'}`}
+          >
+            Subscriptions
+          </li>
         </ul>
       </div>
 
