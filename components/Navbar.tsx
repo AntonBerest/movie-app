7e5b05b98be2542970d4119e169962833f7d4983
AntonBerest <berestovsky.sv@gmail.com>
2026-06-02 01:27:57 +0100

    movie page (adaptive)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     <nav className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between px-16 py-5 bg-black/80 backdrop-blur-sm">
       <div className="text-red-500 font-bold text-xl"> StreamVibe</div>
 
-      <div className="bg-zinc-900 border border-zinc-800 rounded-xl px-2 py-1">
+      <div className="bg-zinc-900 border border-zinc-800 rounded-xl px-8 py-4">
         <ul className="hidden md:flex gap-8 text-white text-sm list-none">
           <li className="cursor-pointer hover:text-red-500">Home</li>
           <li className="cursor-pointer hover:text-red-500">Movies & Shows</li>
