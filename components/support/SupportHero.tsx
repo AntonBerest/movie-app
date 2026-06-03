cc530e28c7645932b2ba9f5316ff72d715333ee9
AntonBerest <berestovsky.sv@gmail.com>
2026-06-03 03:56:10 +0100

    support page (test3)

--- components/support/SupportHero.tsx
+++ components/support/SupportHero.tsx
@@ -12,16 +12,39 @@ export default function SupportHero() {
       </div>
       <div className="grid grid-cols-2 gap-4 bg-[#0F0F0F] border border-zinc-800">
         <div>
-          <p className=""></p>
+          <p className="text-white max-w-xl mb-4">First Name</p>
           <input
             placeholder="Enter First Name"
             className="bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-3 text-white"
           />
         </div>
-        <input
-          className="bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-3 text-white"
-          placeholder="Enter Last Name"
-        />
+        <div>
+          <p className="text-white max-w-xl mb-4">Last Name</p>
+          <input
+            className="bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-3 text-white"
+            placeholder="Enter Last Name"
+          />
+        </div>
+        <div>
+          <p className="text-white max-w-xl mb-4">Email</p>
+          <input
+            placeholder="Enter your Email"
+            className="bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-3 text-white"
+          />
+        </div>
+        <div>
+          <p className="text-white max-w-xl mb-4">Phone Number</p>
+          <div className="grid grid-cols-2 gap-4">
+            <input
+              placeholder="Country"
+              className="bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-3 text-white"
+            />
+            <input
+              placeholder="Enter Last Name"
+              className="bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-3 text-white"
+            />
+          </div>
+        </div>
       </div>
     </div>
   )
