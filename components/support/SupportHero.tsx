98baa50b025884beea397c9fb6021b96b24fd33c
AntonBerest <berestovsky.sv@gmail.com>
2026-06-03 04:18:39 +0100

    support page (test6)

--- components/support/SupportHero.tsx
+++ components/support/SupportHero.tsx
@@ -1,6 +1,6 @@
 export default function SupportHero() {
   return (
-    <div className="mx-4 md:mx-16 mt-24 grid grid-cols-2 gap-4 rounded-xl">
+    <div className="mx-4 md:mx-16 mt-24 grid grid-cols-2 gap-4 ">
       <div>
         <h1 className="text-5xl font-bold mb-4">
           Welcome to our support page!
@@ -10,7 +10,7 @@ export default function SupportHero() {
           product.
         </p>
       </div>
-      <div className="grid grid-cols-2 gap-4 px-4 py-4 bg-[#0F0F0F] border border-zinc-800">
+      <div className="grid grid-cols-2 gap-4 px-4 py-4 bg-[#0F0F0F] border border-zinc-800 rounded-xl">
         <div>
           <p className="text-white max-w-xl mb-4">First Name</p>
           <input
@@ -44,6 +44,17 @@ export default function SupportHero() {
               className="bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-3 text-white w-full"
             />
           </div>
+        </div>
+        <div className="flex">
+          <button></button>
+          <p className="text-zinc-400">
+            I agree with Terms of Use and Privacy Policy
+          </p>
+        </div>
+        <div>
+          <button className="bg-red-700 text-white rounded-lg px-1 md:px-4 py-2">
+            Send Message
+          </button>
         </div>
       </div>
     </div>
