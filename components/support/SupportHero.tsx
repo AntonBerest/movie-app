68a6271d7d029513c848c06a1309e5ddbc27c244
AntonBerest <berestovsky.sv@gmail.com>
2026-06-03 14:19:29 +0100

    support page (test8)

--- components/support/SupportHero.tsx
+++ components/support/SupportHero.tsx
@@ -10,39 +10,41 @@ export default function SupportHero() {
           product.
         </p>
       </div>
-      <div className="grid grid-cols-2 gap-4 px-4 py-4 bg-[#0F0F0F] border border-zinc-800 rounded-xl">
-        <div>
-          <p className="text-white max-w-xl mb-4">First Name</p>
-          <input
-            placeholder="Enter First Name"
-            className="bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-3 text-white w-full"
-          />
-        </div>
-        <div>
-          <p className="text-white max-w-xl mb-4">Last Name</p>
-          <input
-            className="bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-3 text-white w-full"
-            placeholder="Enter Last Name"
-          />
-        </div>
-        <div className="mt-6">
-          <p className="text-white max-w-xl mb-4">Email</p>
-          <input
-            placeholder="Enter your Email"
-            className="bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-3 text-white w-full"
-          />
-        </div>
-        <div className="mt-6">
-          <p className="text-white max-w-xl mb-4">Phone Number</p>
-          <div className="grid grid-cols-2 gap-4">
+      <div className="px-4 py-4 bg-[#0F0F0F] border border-zinc-800 rounded-xl">
+        <div className="grid grid-cols-2 gap-4 ">
+          <div>
+            <p className="text-white max-w-xl mb-4">First Name</p>
             <input
-              placeholder="Country"
-              className="bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-3 text-white"
-            />
-            <input
-              placeholder="Enter Last Name"
+              placeholder="Enter First Name"
               className="bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-3 text-white w-full"
             />
+          </div>
+          <div>
+            <p className="text-white max-w-xl mb-4">Last Name</p>
+            <input
+              className="bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-3 text-white w-full"
+              placeholder="Enter Last Name"
+            />
+          </div>
+          <div className="mt-6">
+            <p className="text-white max-w-xl mb-4">Email</p>
+            <input
+              placeholder="Enter your Email"
+              className="bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-3 text-white w-full"
+            />
+          </div>
+          <div className="mt-6">
+            <p className="text-white max-w-xl mb-4">Phone Number</p>
+            <div className="grid grid-cols-2 gap-4">
+              <input
+                placeholder="Country"
+                className="bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-3 text-white"
+              />
+              <input
+                placeholder="Enter Phone Number"
+                className="bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-3 text-white w-full"
+              />
+            </div>
           </div>
         </div>
         <div>
@@ -52,16 +54,18 @@ export default function SupportHero() {
             className="w-full bg-zinc-900 border border-zinc-800 rounded-lg px-4 py-3 text-white h-32 resize-none"
           />
         </div>
-        <div className="flex mt-6">
-          <button></button>
-          <p className="text-zinc-400">
-            I agree with Terms of Use and Privacy Policy
-          </p>
-        </div>
-        <div className="mt-6">
-          <button className="bg-red-700 text-white rounded-lg px-1 md:px-4 py-2">
-            Send Message
-          </button>
+        <div className="grid grid-cols-2 gap-4 ">
+          <div className="flex mt-6">
+            <button></button>
+            <p className="text-zinc-400">
+              I agree with Terms of Use and Privacy Policy
+            </p>
+          </div>
+          <div className="mt-6">
+            <button className="bg-red-700 text-white rounded-lg px-1 md:px-4 py-2">
+              Send Message
+            </button>
+          </div>
         </div>
       </div>
     </div>
